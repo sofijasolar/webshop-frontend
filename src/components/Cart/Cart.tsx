9a6318515b929595c3202be52c0ec361607e27f1
sofijasolar <sofija.petra@gmail.com>
2024-08-27 13:26:04 +0200

    implemented remove product in cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -4,7 +4,7 @@ import ProductItem from "../Products/ProductItem";
 
 const Cart: React.FC = () => {
 
-    const {productsInCart} = useStoreContext();
+    const {productsInCart, setProductsInCart} = useStoreContext();
     const [totalPrice, setTotalPrice] = useState<number>(0);
 
     const getTotalPrice = () => {
@@ -13,16 +13,26 @@ const Cart: React.FC = () => {
         setTotalPrice(total);
     }
 
+    const removeProduct = (id: number) => {
+        const newProducts = productsInCart.filter((product) => product.id !== id);
+        setProductsInCart(newProducts);
+    }
+
     useEffect(() => {
         getTotalPrice();
-        }, [])
+        }, [productsInCart])
 
         
     return (
         <div className="">
             <p>Total price: $ {totalPrice.toFixed(2) }</p>
             {productsInCart.map((product) => 
-            <ProductItem key={product.id} product={product} />)
+            <div>
+                <ProductItem key={product.id} product={product} />
+                <button onClick={() => removeProduct(product.id)} className="bg-red-300">Remove</button>
+            </div>
+            
+        )
 
              }
 
